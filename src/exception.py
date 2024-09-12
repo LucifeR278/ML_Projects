8e0f31b398f349854a4365394931a62ea4b766ef
LucifeR278 <ashishchamp323@gmail.com>
2024-09-12 09:25:36 +0530

    third commit

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
@@ -15,8 +15,3 @@ class CustomException(Exception):
         return self.error_message
 
 
-try:
-    a=1/0
-except Exception as e:
-    logging.info("Divide by Zero")
-    raise CustomException(e,sys)
